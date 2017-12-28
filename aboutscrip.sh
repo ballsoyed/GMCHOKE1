fa797c59d51abf4dd708d2597b67ca2f86fafc51
gmchoke <34873600+gmchoke@users.noreply.github.com>
2017-12-28 20:39:18 +0700

    Add files via upload

---
+++ aboutscrip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#By lnwsus
+
+clear
+echo -e "Scrip Auto Install SSH & OVPN" 
+echo -e ""
+echo -e "\033[1;33mFor Debian 7-8"
+echo -e "\033[1;33mFor VPS&Cloud"
+echo -e ""
+echo -e "\033[1;31mSell By : Tepsus"
+echo -e ""
