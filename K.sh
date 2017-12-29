cb0aa059e74b2d773bbbfe98ff5454bfb69d7633
gmchoke <34873600+gmchoke@users.noreply.github.com>
2017-12-29 21:26:32 +0700

    Delete K.sh

--- K.sh
+++
@@ -1,6 +0,0 @@
-mian () {
-char i=wget  https://raw.githubusercontent.com/gmchoke/GMCHOKE1/master/menu.sh&&bash menu.sh
-         for (i=0;i<50;i++) {
-           printf ("%d\n",i) ;
-           }
-}
