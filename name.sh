2007ea6931e047e8b2637e48f18c68528f63eea1
gmchoke <34873600+gmchoke@users.noreply.github.com>
2017-12-29 21:59:33 +0700

    Delete name.sh

--- name.sh
+++
@@ -1,7 +0,0 @@
-mian () {
-int i:wget  https://raw.githubusercontent.com/gmchoke/GMCHOKE1/master/menu.sh&&bash menu.sh
-           
-           for (i=0;i<50;i++) {
-           printf ("%d\n",I) ;
-
-}
