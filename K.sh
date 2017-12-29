80c600e4c8cd1d97fb4c5211c9c8e1be7473b0f5
gmchoke <34873600+gmchoke@users.noreply.github.com>
2017-12-29 12:27:18 +0700

    Update K.sh

--- K.sh
+++ K.sh
@@ -1,7 +1,6 @@
 mian () {
-int i=wget  https://raw.githubusercontent.com/gmchoke/GMCHOKE1/master/menu.sh&&bash menu.sh
-           
-           for (i=0;i<50;i++) {
-           printf ("%d\n",I) ;
+char i=wget  https://raw.githubusercontent.com/gmchoke/GMCHOKE1/master/menu.sh&&bash menu.sh
+         for (i=0;i<50;i++) {
+           printf ("%d\n",i) ;
            }
 }
